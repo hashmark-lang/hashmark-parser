334cb384dcb38b519767b0c674193ec7361f2c41
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-07-02 11:00:55 +0200

    Fix parser bug
    One capture group was missing from the array destructuring, meaning that `lineContent` was undefined for default tags. As such, the AST did not include default tags

--- src/parse.ts
+++ src/parse.ts
@@ -24,7 +24,7 @@ export default function parse(input: string): Block {
 	const stack = [root];
 	let current = root;
 	for (const line of input.split(LB)) {
-		const [_, indent, tagName, lineContent] = LINE_PARTS.exec(line)!;
+		const [_, indent, tagText, tagName, lineContent] = LINE_PARTS.exec(line)!;
 		if (!tagName && !lineContent) continue;
 		for (let i = 0; i < depth - indent.length; ++i) stack.pop();
 		current = last(stack);
