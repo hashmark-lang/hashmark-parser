cc29af547d47dd6bfb17ff52f3be5c1667e5d39a
Matthieu Bovel <matthieu@bovel.net>
2019-07-04 16:51:00 +0200

    Make inlineDelimiters a field and defaultSchema an object

--- src/schema.ts
+++ src/schema.ts
@@ -1,31 +1,29 @@
 import { Block, Line } from "./ast";
 
 export interface Schema {
+	inlineDelimiters: { [delimiter: string]: string };
 	getDefault(parentName: string): string;
 	validateBlock(tree: Block): Error[];
 	validateLine(tree: Line): Error[];
 	isRaw(name: string): boolean;
-	getInlineDelimiters(): string[];
 }
 
-export class AnySchema implements Schema {
+export const defaultSchema = {
+	inlineDelimiters: ["**", "*", "__", "_", "$$", "$", "`"],
+
 	getDefault(parentName: string) {
 		return "default";
-	}
+	},
 
 	isRaw(name: string): boolean {
 		return name === "raw";
-	}
+	},
 
 	validateBlock(tree: Block): Error[] {
 		return [];
-	}
+	},
 
 	validateLine(tree: Line): Error[] {
 		return [];
 	}
-
-	getInlineDelimiters() {
-		return ["**", "*", "__", "_", "$$", "$", "`"];
-	}
-}
+};
