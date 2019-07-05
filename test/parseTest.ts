8e7bfd977dc21323702374e9d3578273e5221301
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-07-05 11:44:24 +0200

    Move parse test outputs to separate directory

--- test/parseTest.ts
+++ test/parseTest.ts
@@ -3,7 +3,7 @@ import { parse } from "../src/parse";
 import { FileInfos, filesIn } from "./util";
 
 describe("parse()", () => {
-	for (const output of filesIn("test/output", ".json")) {
+	for (const output of filesIn("test/output/parse", ".json")) {
 		it(`works with ${output.name}`, () => {
 			const input = new FileInfos("test/input", output.name, ".hm");
 			assert.deepStrictEqual(parse(input.content), JSON.parse(output.content));
