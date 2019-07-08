6ef4323f622bfd943412e5b74757f8517ad5f59b
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-07-08 18:17:11 +0200

    Add Hashmark tagged literal util for tests

--- test/util.ts
+++ test/util.ts
@@ -1,5 +1,7 @@
 import { readdirSync, readFileSync } from "fs";
 import * as path from "path";
+import { Block } from "../src/ast";
+import { parse } from "../src/parse";
 
 export class FileInfos {
 	readonly path: string;
@@ -21,4 +23,21 @@ export function* filesIn(dir: string, extension: string): IterableIterator<FileI
 export function readInputFile(fileName: string): string {
 	const filePath = path.join("test", "input", fileName);
 	return readFileSync(filePath).toString();
+}
+
+// Tagged literal
+export function hm(literals: TemplateStringsArray, ...placeholders: string[]): Block {
+	let result = "";
+	for (let i = 0; i < placeholders.length; ++i) {
+		result += literals[i];
+		result += placeholders[i];
+	}
+	result += literals[literals.length - 1];
+	const lines = result.split(/\r\n|\n|\r/).filter(line => line.trim().length > 0);
+	if (lines.length <= 1) {
+		return parse(result);
+	}
+	const indentation = lines[0].search(/[^\t]/);
+	const deindented = lines.map(line => line.substring(indentation)).join("\n");
+	return parse(deindented);
 }
