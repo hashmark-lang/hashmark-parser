ba3b4ce089722f754a0ce611f458088d2c0962e6
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-07-09 11:30:17 +0200

    Add AST query functions

--- src/ast.ts
+++ src/ast.ts
@@ -11,3 +11,22 @@ export interface Inline {
 	arguments: Line[];
 	closed: boolean;
 }
+
+export function queryAll(ast: Block, tag: string): Block[] {
+	return queryAllChildren(ast, tag).concat(ast.children.flatMap(child => queryAll(child, tag)));
+}
+
+export function queryAllChildren(ast: Block, tag: string): Block[] {
+	return ast.children.filter(b => b.tag === tag);
+}
+
+export function query(ast: Block, tag: string): Block | undefined {
+	return (
+		queryChildren(ast, tag) ||
+		ast.children.reduce((found, child) => found || query(child, tag), undefined)
+	);
+}
+
+export function queryChildren(ast: Block, tag: string): Block | undefined {
+	return ast.children.find(b => b.tag === tag);
+}
