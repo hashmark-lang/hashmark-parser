1e57c6bd627426770bbddfc546c5df5a78a0adab
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-07-15 15:05:52 +0200

    Add missing exports to index.ts barrel

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
+export * from "./ast";
+export * from "./errors";
+export * from "./InlineParser";
+export * from "./json";
 export * from "./Parser";
+export * from "./parseSchema";
 export * from "./xml";
