b222a2b175fdfd564404f32d5c75589f4f2357a5
Matthieu Bovel <matthieu@bovel.net>
2019-07-18 23:02:17 +0200

    Split Schema interface

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -1,6 +1,35 @@
 // Additional rule:
 //   All prop names within an element must be different
 
+export interface Schema {
+	root: Omit<BlockSchema, "tag" | "head">;
+	blockElements: BlockSchema[];
+	inlineElements: InlineSchema[];
+}
+
+export interface BlockSchema {
+	defaultTag?: string;
+	invalidTag?: string;
+	props: [RawBlockProp] | BlockProp[];
+	tag: string;
+	head?: InlineProp;
+}
+
+export interface RawBlockProp {
+	name: string;
+	raw: true;
+}
+
+export interface BlockProp {
+	name: string;
+	raw: undefined;
+	content: Array<{
+		schema: string;
+		tag: string;
+		cardinality: Cardinality;
+	}>;
+}
+
 export const enum Cardinality {
 	ZeroOrMore = "zeroOrMore",
 	OneOrMore = "oneOrMore",
@@ -8,41 +37,28 @@ export const enum Cardinality {
 	Optional = "optional"
 }
 
-interface BlockSchema {
-	default?: string;
-	props:
-		| [{ name: string; content: "raw" }]
-		| Array<{
-				name: string;
-				content: {
-					[tag: string]: Cardinality;
-				};
-		  }>;
+export interface InlineSchema {
+	tag: string;
+	props: InlineProp[];
+	sugar?: Sugar;
 }
 
-export interface Schema {
-	root: BlockSchema;
-
-	blockElements: {
-		[tag: string]: BlockSchema & {
-			head?: {
-				name: string;
-				content: "raw" | string[];
-			};
-		};
-	};
-
-	inlineElements: {
-		[tag: string]: {
-			props: Array<{
-				name: string;
-				content: "raw" | string[];
+export type InlineProp =
+	| {
+			name: string;
+			raw: true;
+	  }
+	| {
+			name: string;
+			raw: undefined;
+			content: Array<{
+				schema: string;
+				tag: string;
 			}>;
-			sugar?: {
-				start: string;
-				separator?: string;
-				end: string;
-			};
-		};
-	};
+	  };
+
+export interface Sugar {
+	start: string;
+	separator?: string;
+	end: string;
 }
