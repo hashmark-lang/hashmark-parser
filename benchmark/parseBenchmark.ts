8110387155def34a33380d08cdbb87aeda5a8e6a
Matthieu Bovel <matthieu@bovel.net>
2019-07-25 17:00:30 +0200

    Fix benchmarks

--- benchmark/parseBenchmark.ts
+++ benchmark/parseBenchmark.ts
@@ -1,7 +1,59 @@
 /* tslint:disable:no-console */
-import { Event, Suite } from "benchmark";
+import * as Benchmark from "benchmark";
+import { BlockElement, BlockParser } from "../src";
+import { AstHandler } from "../src/ast/AstHandler";
 import { toJSON } from "../src/output/json";
-import { parse } from "../src/parser/Parser";
+
+(function run() {
+	const input = generateBenchmarkInput();
+	console.log("Size of test input: " + formatBytes(input.length));
+	const jsonAst = JSON.stringify(toJSON(parse(input)));
+	new Benchmark.Suite("compare")
+		.add("Parse Hashmark", () => {
+			parse(input);
+		})
+		.add("Parse JSON", () => {
+			JSON.parse(jsonAst);
+		})
+		.add("String.matchAll", () => {
+			for (const token of input.matchAll(
+				/(?:((?:\r\n|\n|\r|^)(\t*)[\t ]*(?:#([^ \[\r\n]+)(?: |$))?)|(#([^ \[]+)(\[)?)|(]\[)|(\[)|(\\(.)))/g
+			));
+		})
+		.add("Iterate through chars", () => {
+			const SHARP = "#";
+			const OPENING_BRACKET = "[";
+			const end = input.length;
+			for (let i = 0; i < end; ++i) {
+				switch (input.charAt(i)) {
+					case SHARP:
+						break;
+					case OPENING_BRACKET:
+						break;
+				}
+			}
+		})
+		// add listeners
+		.on("cycle", (event: Event) => {
+			console.log(String(event.target));
+		})
+		.on("complete", function(this: Benchmark[]) {
+			console.log(
+				"JSON/Hashmark ratio: " + (this[1].stats.mean / this[0].stats.mean).toFixed(2)
+			);
+		})
+		// run async
+		.run({ async: true });
+})();
+
+const handler = new AstHandler();
+const parser = new BlockParser(handler);
+
+function parse(input: string): BlockElement {
+	handler.reset();
+	parser.parse(input);
+	return handler.getResult();
+}
 
 const N_CHILDREN = 10;
 const MAX_DEPTH = 3;
@@ -29,41 +81,3 @@ function formatBytes(bytes: number, decimals = 2) {
 
 	return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 }
-
-const input = generateBenchmarkInput();
-console.log("Size of test input: " + formatBytes(input.length));
-const jsonAst = JSON.stringify(toJSON(parse(input)));
-new Suite("compare")
-	.add("Parse Hashmark", () => {
-		parse(input);
-	})
-	.add("Parse JSON", () => {
-		JSON.parse(jsonAst);
-	})
-	.add("String.matchAll", () => {
-		for (const token of input.matchAll(
-			/(?:((?:\r\n|\n|\r|^)(\t*)[\t ]*(?:#([^ \[\r\n]+)(?: |$))?)|(#([^ \[]+)(\[)?)|(]\[)|(\[)|(\\(.)))/g
-		));
-	})
-	.add("Iterate through chars", () => {
-		const SHARP = "#";
-		const OPENING_BRACKET = "[";
-		const end = input.length;
-		for (let i = 0; i < end; ++i) {
-			switch (input.charAt(i)) {
-				case SHARP:
-					break;
-				case OPENING_BRACKET:
-					break;
-			}
-		}
-	})
-	// add listeners
-	.on("cycle", (event: Event) => {
-		console.log(String(event.target));
-	})
-	.on("complete", function() {
-		console.log("JSON/Hashmark ratio: " + (this[1].stats.mean / this[0].stats.mean).toFixed(2));
-	})
-	// run async
-	.run({ async: true });
