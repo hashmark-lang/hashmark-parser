2a40d63e7e9b0c4180aa3c56765a12c771816c71
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-08-20 15:55:01 +0200

    Allow emptyBlockProps() to take readonly arrays
    ReadonlyArray is a more general type than Array

--- src/ir/IRNode.ts
+++ src/ir/IRNode.ts
@@ -7,6 +7,6 @@ export interface IRNode {
 
 export type IRNodeList = Array<string | IRNode>;
 
-export function emptyBlockProps(propNames: string[]): { [key: string]: [] } {
+export function emptyBlockProps(propNames: ReadonlyArray<string>): { [key: string]: [] } {
 	return Object.fromEntries(propNames.map(x => [x, []]));
 }
