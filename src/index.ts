e1e777032f9a43ee37f63f5b407dcddd175f5c7f
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-08-28 15:16:58 +0200

    Export Schema and typescript in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-export * from "./schema/errors";
-export * from "./parser/InlineParser";
 export * from "./ir/IRNode";
+
 export * from "./schema/errors";
-export * from "./schema/SchemaDefinition";
+export * from "./schema/Schema";
 export * from "./schema/schema-generators";
+export * from "./schema/SchemaDefinition";
+export * from "./schema/typescript";
+
 export * from "./parser/parse";
 export * from "./parser/BlockParser";
+export * from "./parser/InlineParser";
 export * from "./parser/InputPosition";
+
 export * from "./output/xml";
