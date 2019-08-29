e378b42654c7e31090488bb2a49ab3c2e9e1cc59
Maxime Kjaer <maxime.kjaer@gmail.com>
2019-08-29 15:40:16 +0200

    Add schema schema interface compilation script
    Format twice as a workaround to palantir/tslint#4844

---
+++ compileSchemaSchema.ts
@@ -0,0 +1,18 @@
+// tslint:disable:no-console
+import { writeFileSync } from "fs";
+import { convertSchemaToTypescript, Schema, schemaSchema } from "./src";
+
+const path = process.argv[2];
+if (!path) {
+	console.error("Usage: ts-node compileSchemaSchema.ts <path>");
+	console.error("Expected a path argument");
+	process.exit(1);
+}
+
+try {
+	const output = convertSchemaToTypescript(new Schema(schemaSchema));
+	writeFileSync(path, output);
+} catch (e) {
+	console.error(e);
+	process.exit(1);
+}
