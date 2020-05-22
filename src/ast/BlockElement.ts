cc0cf77565430e0bc3d2324c890ba9fb2a64322d
Matthieu Bovel <matthieu@bovel.net>
2020-05-22 22:52:59 +0200

    Remove block errors

--- src/ast/BlockElement.ts
+++ src/ast/BlockElement.ts
@@ -11,19 +11,6 @@ export class BlockElement {
 		readonly tagEndColumn: number = 0,
 		readonly labelStartColumn: number = 0,
 		readonly labelEndColumn: number = 0,
-		readonly errors: BlockError[] = [],
 		readonly children: BlockElement[] = []
 	) {}
-
-	get(tag: string): BlockElement | undefined {
-		return this.children.find(child => child.tag === tag);
-	}
-
-	getAll(tags: ReadonlySet<string | null>): BlockElement[] {
-		return this.children.filter(child => tags.has(child.tag));
-	}
-
-	addError(message: string, startColumn: number = this.tagStartColumn, endColumn: number = this.tagStartColumn) {
-		this.errors.push(new BlockError(message, startColumn, endColumn));
-	}
 }
--- src/ast/BlockError.ts
+++
@@ -1,3 +0,0 @@
-export class BlockError {
-	constructor(readonly message: string, readonly colStart: number, readonly colEnd: number) {}
-}
